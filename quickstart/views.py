a57e0735576e18f4985bc8067bdc1188479cb84a
mahdihma <mahdi.haji@yahoo.com>
2019-11-26 23:00:11 +0330

    view

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User, Group
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+
+from quickstart.serializer import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('username')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
