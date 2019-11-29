80d7f2b9662a7f20d1f94cb0212da104b0cf7849
mahdihma <mahdi.haji@yahoo.com>
2019-11-30 00:55:02 +0330

    snippet individual actions added

--- snippest/views.py
+++ snippest/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -26,5 +26,24 @@ def snippet_list(request):
 
 
 @csrf_exempt
-def snippet_detail():
-    pass
+def snippet_detail(request, pk):
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == "GET":
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    if request.method == "PUT":
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(data)
+        return JsonResponse(serializer.errors, status=400)
+
+    if request.method == "DELETE":
+        snippet.delete()
+        return HttpResponse(status=204)
