fe658c6fd98f57a5f900cd899f0e53555db18a44
mahdihma <mahdi.haji@yahoo.com>
2019-11-29 23:20:53 +0330

    snippet

--- rest/settings.py
+++ rest/settings.py
@@ -35,7 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'snippest.apps.SnippestConfig',
+    'quickstart.apps.QuickstartConfig'
 ]
 
 MIDDLEWARE = [
