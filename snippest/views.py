8d4d2f9114946a89d2f48aa7d38fa3628f1fd168
mahdihma <mahdi.haji@yahoo.com>
2019-11-30 00:43:36 +0330

    snippet view

--- snippest/views.py
+++ snippest/views.py
@@ -1,3 +1,30 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+
+from snippest.models import Snippet
+from snippest.serializer import SnippetSerializer
+
+
+@csrf_exempt
+def snippet_list(request):
+    if request.method == 'GET':
+        snippet = Snippet.objects.all()
+        serializer = SnippetSerializer(snippet, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'POST':
+        data = JSONParser() \
+            .parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def snippet_detail():
+    pass
