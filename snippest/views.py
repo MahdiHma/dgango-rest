158b60ad06de3a55a75e54051e0d3caad00f5290
mahdihma <mahdi.haji@yahoo.com>
2019-12-02 15:57:46 +0330

    view class using generic api views only most clean state

--- snippest/views.py
+++ snippest/views.py
@@ -13,30 +13,40 @@ from snippest.models import Snippet
 from snippest.serializer import SnippetSerializer
 
 
-class SnippetList(mixins.CreateModelMixin, mixins.ListModelMixin, generics.GenericAPIView):
-    serializer_class = SnippetSerializer
-    queryset = Snippet.objects.all()
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetails(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin,
-                     generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, req, *args, **kwargs):
-        return self.retrieve(req, *args, **kwargs)
 
-    def put(self, req, *args, **kwargs):
-        return self.update(req, *args, **kwargs)
+class SnippetDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
 
-    def delete(self, req, *args, **kwargs):
-        return self.delete(req, *args, **kwargs)
+
+# class SnippetList(mixins.CreateModelMixin, mixins.ListModelMixin, generics.GenericAPIView):
+#     serializer_class = SnippetSerializer
+#     queryset = Snippet.objects.all()
+#
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+#
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+#
+#
+# class SnippetDetails(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin,
+#                      generics.GenericAPIView):
+#     queryset = Snippet.objects.all()
+#     serializer_class = SnippetSerializer
+#
+#     def get(self, req, *args, **kwargs):
+#         return self.retrieve(req, *args, **kwargs)
+#
+#     def put(self, req, *args, **kwargs):
+#         return self.update(req, *args, **kwargs)
+#
+#     def delete(self, req, *args, **kwargs):
+#         return self.delete(req, *args, **kwargs)
 
 
 # class SnippetList(APIView):
